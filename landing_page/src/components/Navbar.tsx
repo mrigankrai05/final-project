11d517d52fa07daac15d27a6c1bffbe8283a11ea
Mrigank Rai <mrigankrai05@gmail.com>
2024-11-15 14:30:16 +0530

    added home button

--- landing_page/src/components/Navbar.tsx
+++ landing_page/src/components/Navbar.tsx
@@ -6,12 +6,14 @@ const Navbar = () => {
     <nav className="bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16">
-          <div className="flex items-center">
-            <Stethoscope className="h-8 w-8 text-blue-600" />
-            <span className="ml-2 text-xl font-semibold text-gray-800">
-              HealthCare Plus
-            </span>
-          </div>
+          <NavLink href="/">
+            <div className="flex items-center space-x-3 mt-2">
+              <Stethoscope className="h-8 w-8 text-blue-600" />
+              <span className="ml-2 text-xl font-semibold text-gray-800">
+                HealthCare Plus
+              </span>
+            </div>
+          </NavLink>
           <div className="flex items-center space-x-8">
             <NavLink href="/inventory">Inventory</NavLink>
             <NavLink href="/hospitalfinder">Hospital</NavLink>
